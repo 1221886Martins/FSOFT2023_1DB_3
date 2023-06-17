be760083b29bb52f68ceb5dbc2862bf8da6dcb80
1221886Martins <135598282+1221886Martins@users.noreply.github.com>
2023-06-17 13:40:31 +0100

    Delete FSOFT2023_1DB_3 directory

--- FSOFT2023_1DB_3/headers/Jogo_Especial_Stat/Jogo_Especial_MN.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef UNTITLED2_JOGO_ESPECIAL_MN_H
-#define UNTITLED2_JOGO_ESPECIAL_MN_H
-#include <vector>
-#include <iostream>
-
-void Stat_MN();
-
-#endif //UNTITLED2_JOGO_ESPECIAL_MN_H
--- FSOFT2023_1DB_3/headers/Menu.h
+++
@@ -1,7 +0,0 @@
-#ifndef SCOREBOARD_MENU_H
-#define SCOREBOARD_MENU_H
-
-void menu();
-
-
-#endif //SCOREBOARD_MENU_H
