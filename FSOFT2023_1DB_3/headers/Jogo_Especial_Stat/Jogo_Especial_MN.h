8d928e59c10e715793c068ad837fb08273791a5e
1221886Martins <135598282+1221886Martins@users.noreply.github.com>
2023-06-17 13:54:49 +0100

    Add files via upload

---
+++ FSOFT2023_1DB_3/headers/Jogo_Especial_Stat/Jogo_Especial_MN.h
@@ -0,0 +1,9 @@
+
+#ifndef UNTITLED2_JOGO_ESPECIAL_MN_H
+#define UNTITLED2_JOGO_ESPECIAL_MN_H
+#include <vector>
+#include <iostream>
+
+void Stat_MN();
+
+#endif //UNTITLED2_JOGO_ESPECIAL_MN_H
---
+++ FSOFT2023_1DB_3/headers/Menu.h
@@ -0,0 +1,7 @@
+#ifndef SCOREBOARD_MENU_H
+#define SCOREBOARD_MENU_H
+
+void menu();
+
+
+#endif //SCOREBOARD_MENU_H
